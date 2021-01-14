34e5e04f94f7bb25402f47f6b67cd8b443bdb395
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-14 07:16:15 +0000

    Wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const transpose = function (matrix) {
+        let outerArray = [];
+        for (let i = 0; i < matrix[i].length; i++) {
+            let innerArray = [];
+            for (let j = 0; j < matrix.length; j++) {
+                innerArray.push(matrix[j][i]);
+            }
+            outerArray.push(innerArray);
+        }
+        return outerArray;
+    };
+    const transposedMatrix = transpose(letters);
+    // console.log(transposedMatrix);
+    const verticalJoin = transposedMatrix.map(ls => ls.join(''));
+    // console.log(verticalJoin);
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true
+        }
     }
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    // console.log(horizontalJoin);
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
